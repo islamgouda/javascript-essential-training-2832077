c9c3ec67c25691406081aa12880f522a01668ba0
islamgouda <33799849+islamgouda@users.noreply.github.com>
2022-04-29 16:17:03 +0200

    script.js
    added newName function to update name

--- 03_08/script.js
+++ 03_08/script.js
@@ -19,7 +19,16 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  newName:function(name){
+  this.name=name;
+  }
+  changeColor:function(color){
+  this.color=color;
+  }
 };
 
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
+console.log("name"+backpack.name);
+backpack.newName("Ahmed");
+console.log("New Name"+backpack.name);
